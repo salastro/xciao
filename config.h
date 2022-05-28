4e5aeb6eb848d4e575c94634523b238e3a1f8311
salastro <salatrod@gmail.com>
2022-05-28 23:35:11 +0200

    Add `config.h`

---
+++ config.h
@@ -0,0 +1,15 @@
+static Remap remaps[] = {
+    /*       Class      | Input mask  | input key    | Output mask  | Output key */
+    {"Brave-browser", "ControlMask", "k",           "ControlMask", "Next"}, /* Next tab */
+    {"Brave-browser", "ControlMask", "j",           "ControlMask", "Prior"},/* Previous tab */
+    {"Brave-browser", "ControlMask", "h",           "Mod1Mask",    "Home"},  /* Control-h -> Home page */
+    {"Brave-browser", "ControlMask", "d",           "",            "Next"}, /* Page down */
+    {"Brave-browser", "ControlMask", "u",           "",            "Prior"},/* Page up */
+    {"Brave-browser", "Mod1Mask",    "h",           "",            "Left"}, /* Left arrow key */
+    {"Brave-browser", "Mod1Mask",    "j",           "",            "Down"}, /* Down arrow key */
+    {"Brave-browser", "Mod1Mask",    "k",           "",            "Up"},   /* Up arrow key */
+    {"Brave-browser", "Mod1Mask",    "l",           "",            "Right"},/* Right arrow key */
+    {"Brave-browser", "ControlMask", "bracketleft", "Mod1Mask",    "Left"}, /* Backwards history */
+    {"Brave-browser", "ControlMask", "bracketright","Mod1Mask",    "Right"},/* Forwards history */
+};
+

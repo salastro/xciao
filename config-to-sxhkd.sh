10ab764b1428a25d8118ee5eae3bd333f4b93516
salastro <salatrod@gmail.com>
2022-05-29 13:15:45 +0200

    config-to-sxhkd: fix empty lines and duplicates

--- config-to-sxhkd.sh
+++ config-to-sxhkd.sh
@@ -5,6 +5,13 @@ FILE="sxhkd"
 
 cp $CONFIG $FILE
 
+sed -i '1,2d;$d' $FILE
+sed -i '$d' $FILE
+sed -i -r '/^\s*$/d' $FILE
 vim -n -u NONE -s macro1.vim $FILE
+sort -r -u -o $FILE $FILE
 sed -i 'p' $FILE
+echo >> $FILE
 vim -n -u NONE -s macro2.vim $FILE
+
+# cat $FILE
--- macro1.vim
+++ macro1.vim
@@ -1,6 +1,5 @@
-djGdkVgg:norm df,$d3F,x
+ggVG:norm df,$d3F,x
 :%s/,\s*/ 
 ggVG:norm Ixciao 
 0ggr	
-:norm Go
 :wq
